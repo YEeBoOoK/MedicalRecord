70386695582246b93714aaae3bd712720b43848d
Елена Османова <elenaosmanova2003@gmail.com>
2022-12-20 18:52:38 +0300

    V8

--- config/db.php
+++ config/db.php
@@ -4,7 +4,7 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=638-19_MedicalRecord',
     'username' => '638-19_Aboba',
-    'password' => 'partK666.',
+    'password' => 'PKGH666.', //не настоящий парол
     'charset' => 'utf8'
     // Schema cache options (for production environment)
     //'enableSchemaCache' => true,
